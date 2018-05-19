9484e16ece1c4b2dd627b4c1c57a969eb04e8d6c
DanielJ <danielwjameson@yahoo.com>
2018-05-19 06:25:41 -0500

    Remove useless using statments

--- DocHelper/Formatter.cs
+++ DocHelper/Formatter.cs
@@ -1,11 +1,7 @@
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DocHelper {
 	class Formatter {
