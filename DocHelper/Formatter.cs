edcadf0c15b627bb3d7472975e3628f2eb92bc17
DanielJ <dnlj@users.noreply.github.com>
2024-02-18 15:09:41 -0600

    Half-baked update to improve compatibility with other extension and avoid inserting extra asterisks

--- DocHelper/Formatter.cs
+++ DocHelper/Formatter.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using System;
+//using System.Diagnostics;
 
 namespace DocHelper {
 	class Formatter {
@@ -32,9 +33,12 @@ namespace DocHelper {
 			}
 		}
 
+		// TODO: This whole function is a mess. I think we should be able to
+		//       clean this up a lot by utilizing change.OldText/NewText and other
+		//       ITextChange members.
 		private void HandleChange(ITextChange change) {
 			// Skip invalid changes
-			if (!IsNewLine(change.NewText[0])) { return; }
+			if (change.NewText.Length == 0 || !IsNewLine(change.NewText[0])) { return; }
 
 			// Useful variables
 			var line = view.TextSnapshot.GetLineFromPosition(change.OldPosition);
@@ -42,6 +46,8 @@ namespace DocHelper {
 			var lineTextTrimmed = lineText.TrimStart();
 			int indentIndex = lineText.Length - lineTextTrimmed.Length;
 			var cursorPos = change.OldPosition - line.Start.Position;
+			//Debug.WriteLine(change.ToString());
+			//System.Diagnostics.Debugger.Break();
 
 			// Determine where we are in the comment
 			var inComment = false;
@@ -66,34 +72,46 @@ namespace DocHelper {
 			var edit = view.TextBuffer.CreateEdit();
 
 			if (afterIndent) {
-				var afterCursor = lineText.Substring(cursorPos);
-				var afterCursorTrim = afterCursor.TrimStart();
 
 				string append = isCommentStart ? " * " : "* ";
 
-				if (afterCursorTrim.StartsWith("*")) {
-					// Dont add an extra asterisk if there is already one there
-					append = append.Substring(0, append.Length - 2);
-					var afterLength = afterCursor.Length - afterCursorTrim.Length;
-					edit.Delete(change.NewEnd, afterLength);
+				//Debug.WriteLine("|"+change.NewText+"|");
 
-					// Add a space after if needed
-					var nextCharIndex = cursorPos + afterLength + 1;
-					char nextChar = '\0';
+				// TODO: When can this case happen? Seems like its never hit.
+				//
+				//var afterCursor = lineText.Substring(cursorPos);
+				//var afterCursorTrim = afterCursor.TrimStart();
+				//
+				//if (afterCursorTrim.StartsWith("*")) {
+				//	Debug.WriteLine("AFTER !!!!!!!!");
+				//	// Dont add an extra asterisk if there is already one there
+				//	append = append.Substring(0, append.Length - 2);
+				//	var afterLength = afterCursor.Length - afterCursorTrim.Length;
+				//	edit.Delete(change.NewEnd, afterLength);
+				//
+				//	// Add a space after if needed
+				//	var nextCharIndex = cursorPos + afterLength + 1;
+				//	char nextChar = '\0';
+				//
+				//	if (nextCharIndex < lineText.Length) {
+				//		nextChar = lineText[nextCharIndex];
+				//	}
+				//
+				//	if (nextChar != '/' && !char.IsWhiteSpace(nextChar)) {
+				//		edit.Insert(change.NewEnd + afterLength + 1, " ");
+				//	}
+				//}
 
-					if (nextCharIndex < lineText.Length) {
-						nextChar = lineText[nextCharIndex];
-					}
-
-					if (nextChar != '/' && !char.IsWhiteSpace(nextChar)) {
-						edit.Insert(change.NewEnd + afterLength + 1, " ");
-					}
+				if (!change.NewText.Contains("*")) {
+					//Debug.WriteLine("Insert: |"+ append +"|");
+					edit.Insert(change.NewEnd, lineText.Substring(0, indentIndex) + append);
 				}
-
-				edit.Insert(change.NewEnd, lineText.Substring(0, indentIndex) + append);
 			} else {
-				edit.Insert(change.OldPosition, lineText.Substring(cursorPos, indentIndex - cursorPos) + "* ");
-				edit.Insert(change.NewEnd, lineText.Substring(0, cursorPos));
+				// TODO: what was the point of this branch? Doesn't seem useful?
+				//Debug.WriteLine("Insert 2");
+				//Debug.WriteLine(change);
+				//edit.Insert(change.OldPosition, lineText.Substring(cursorPos, indentIndex - cursorPos) + "* ");
+				//edit.Insert(change.NewEnd, lineText.Substring(0, cursorPos));
 			}
 
 			edit.Apply();
